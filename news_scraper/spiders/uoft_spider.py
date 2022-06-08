800370aa395e1d774f140a162d6494e104801486
Parth Gupta <parthgupta023@gmail.com>
2022-06-07 22:26:29 -0400

    Scraped titles

--- news_scraper/spiders/uoft_spider.py
+++ news_scraper/spiders/uoft_spider.py
@@ -6,6 +6,11 @@ class UofTSpider(scrapy.Spider):
     start_urls = ["https://www.utoronto.ca/news/searchnews/"]
 
     # We need to get all news docs
-
     def parse(self, response):
-        pass
+        content = response.css("div.view-content")
+        news_articles = content.css("div.panel-col-last")
+
+        for article in news_articles:
+            yield {
+                "name": article.css("h4.field-content a::text").get()
+            }
