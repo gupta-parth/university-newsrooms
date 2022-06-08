e599ca1b4e1811f129015afb5ed76a6cd6443995
Parth Gupta <parthgupta023@gmail.com>
2022-06-07 22:54:20 -0400

    Worked on uoft script

--- news_scraper/spiders/uoft_spider.py
+++ news_scraper/spiders/uoft_spider.py
@@ -9,8 +9,18 @@ class UofTSpider(scrapy.Spider):
     def parse(self, response):
         content = response.css("div.view-content")
         news_articles = content.css("div.panel-col-last")
+        news_article_links = news_articles.css(
+            "h4.field-content a::attr(href)")
+        yield from response.follow_all(news_article_links, self.parse_news_doc)
 
-        for article in news_articles:
-            yield {
-                "name": article.css("h4.field-content a::text").get()
-            }
+    def parse_news_doc(self, response):
+        def clean_text(query):
+            text_list = response.css(query).getall()
+            text = (' ').join(text_list)
+            text = text.replace('\xa0', '')
+            return text.strip()
+
+        yield {
+            "name": response.css("div.pane-content h1::text").get().strip(),
+            "content": clean_text("div.row div.pane-content p::text")
+        }
