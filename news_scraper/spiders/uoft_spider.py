063dfc69146366fe64c72028bb8cebebb42f32d8
Parth Gupta <parthgupta023@gmail.com>
2022-06-08 16:00:36 -0400

    Scraped data

--- news_scraper/spiders/uoft_spider.py
+++ news_scraper/spiders/uoft_spider.py
@@ -5,7 +5,6 @@ class UofTSpider(scrapy.Spider):
     name = "uoft"
     start_urls = ["https://www.utoronto.ca/news/searchnews/"]
 
-    # We need to get all news docs
     def parse(self, response):
         content = response.css("div.view-content")
         news_articles = content.css("div.panel-col-last")
@@ -13,6 +12,10 @@ class UofTSpider(scrapy.Spider):
             "h4.field-content a::attr(href)")
         yield from response.follow_all(news_article_links, self.parse_news_doc)
 
+        pagination_link = response.css("li.pager-next a")
+        yield from response.follow_all(pagination_link, self.parse)
+
+    # Function for parsing each news document separately
     def parse_news_doc(self, response):
         def clean_text(query):
             text_list = response.css(query).getall()
@@ -23,6 +26,6 @@ class UofTSpider(scrapy.Spider):
         yield {
             "Name": response.css("div.pane-content h1::text").get().strip(),
             "Date": response.css("div.date::text").get(),
-            "Content": clean_text("div.row div.pane-content p::text"),
+            "Content": clean_text("div.row div.pane-content p::text, strong::text"),
             "Tags": response.css("div.Tags div.pane-content a::text").getall()
         }
